0d18148697727e4e8bed525a54d4943e060a9f90
diana-vahomskaya <diana.vahomskaya@mail.ru>
2022-09-11 14:31:06 +0300

    Добавлены задачи. Первая решена идеально, тесты прошли, к последним трем добавлен ввод параметров. К задаче с монетами решение некоторых условий

---
+++ Cards/Cards/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Programm
+{
+    static int count;
+    static int[] numbers;
+    static int PSum;
+    static int VSUm;
+
+    public static void Main(string[] args)
+    {
+        count = Convert.ToInt16(Console.ReadLine());
+        numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .ToArray();
+        PSum = 0;
+        VSUm = 0;
+        for (int i = 0; i < count; i += 3)
+        {
+            PSum += numbers[i];
+            VSUm += numbers[i + 1];
+
+            if (numbers[i] < numbers[i + 1])
+                PSum += numbers[i + 2];
+            else
+                VSUm += numbers[i + 2];
+        }
+        if (VSUm > PSum)
+        {
+            Console.WriteLine("Vasya");
+        }
+        else
+        {
+            Console.WriteLine("Petya");
+        }
+    }
+}
